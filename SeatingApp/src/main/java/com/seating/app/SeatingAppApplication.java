b1b011810a2685290bd93bab9990a0deac6ce09f
manikanta.s <manikanta.s@LP-5CD7465HZF.HCLT.CORP.HCL.IN>
2018-06-10 16:42:14 +0530

    Seat booking app commit

---
+++ SeatingApp/src/main/java/com/seating/app/SeatingAppApplication.java
@@ -0,0 +1,12 @@
+package com.seating.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatingAppApplication.class, args);
+	}
+}
