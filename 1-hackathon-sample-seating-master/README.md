cd74cefe80034760cd6d5446cc6a81e23e6e3bd5
manikanta.s <manikanta.s@LP-5CD7465HZF.HCLT.CORP.HCL.IN>
2018-06-10 16:36:26 +0530

    removing hackathon sample project

--- 1-hackathon-sample-seating-master/README.md
+++
@@ -1,8 +0,0 @@
-# Introduction
-
-This is a introduction sample exercise project, that demonstrate basic concepts like,
-
-- Spring Boot
-- Heroku deployment
-- Flyway
-
--- seat-booking/src/main/java/com/krishna/seatbooking/repository/SectionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.krishna.seatbooking.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.krishna.seatbooking.dto.Section;
-
-public interface SectionRepository extends JpaRepository<Section, Long> {
-}
